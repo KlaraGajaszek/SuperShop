1dfa4c20e71bfbdf6db790222a3a22bea653ec95
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-10-03 20:12:48 +0200

    change type

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import Header from './components/moleculs/Header/Header';
 import StartView from '../src/views/StartView/StartView';
 
+export enum HeaderTitle {
+  title1 = 'title1',
+  title2 = 'title2',
+  title3 = 'title3',
+  title4 = 'title4',
+}
+
+export enum HeaderLinkTitle {
+  link1 = 'title1',
+  link2 = 'title2',
+  link3 = 'title3',
+  link4 = 'title4',
+}
+
 const App = () => {
   return (
     <>
       <Header
-        title1={'title1'}
-        title2={'title2'}
-        title3={'title3'}
-        title4={'title4'}
+        title1={HeaderTitle.title1}
+        title2={HeaderTitle.title2}
+        title3={HeaderTitle.title3}
+        title4={HeaderTitle.title4}
+        link1={HeaderLinkTitle.link1}
+        link2={HeaderLinkTitle.link2}
+        link3={HeaderLinkTitle.link3}
+        link4={HeaderLinkTitle.link4}
       />
       <StartView />
     </>
--- src/components/moleculs/Header/Header.tsx
+++ src/components/moleculs/Header/Header.tsx
@@ -2,29 +2,42 @@ import React, { FC } from 'react';
 import { ReactComponent as Supershop } from '../../../assests/SUPERSHOP.svg';
 import { HeaderBlock, LiItem, NavbarContainerUl, AItem } from './HeaderStyles';
 
-type Titles = {
-  title1: string;
-  title2: string;
-  title3: string;
-  title4: string;
-};
-
-const Header: FC<Titles> = ({ title1, title2, title3, title4 }) => {
+type Titles = Record<
+  | 'title1'
+  | 'title2'
+  | 'title3'
+  | 'title4'
+  | 'link1'
+  | 'link2'
+  | 'link3'
+  | 'link4',
+  string
+>;
+const Header: FC<Titles> = ({
+  title1,
+  title2,
+  title3,
+  title4,
+  link1,
+  link2,
+  link3,
+  link4,
+}) => {
   return (
     <HeaderBlock>
       <Supershop />
       <NavbarContainerUl>
         <LiItem>
-          <AItem href="default.asp">{title1}</AItem>
+          <AItem href={link1}>{title1}</AItem>
         </LiItem>
         <LiItem>
-          <AItem href="default.asp">{title2}</AItem>
+          <AItem href={link2}>{title2}</AItem>
         </LiItem>
         <LiItem>
-          <AItem href="default.asp">{title3}</AItem>
+          <AItem href={link3}>{title3}</AItem>
         </LiItem>
         <LiItem>
-          <AItem href="default.asp">{title4}</AItem>
+          <AItem href={link4}>{title4}</AItem>
         </LiItem>
       </NavbarContainerUl>
     </HeaderBlock>
