8d2bc777779cf19519419bb83a9a383cc2543897
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-10-21 19:56:10 +0200

    change to the class components

--- src/components/atoms/Tag/Tag.tsx
+++ src/components/atoms/Tag/Tag.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
+
 import { TagStyles } from './TagStyles';
 
 type TagProps = {
   title: string;
 };
+
 const Tag: FC<TagProps> = ({ title }) => {
   return <TagStyles>{title}</TagStyles>;
 };
