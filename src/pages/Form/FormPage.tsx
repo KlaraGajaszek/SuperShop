2e6cea36c88b3053a32a67fb79490831e0c5e324
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-10-22 14:31:22 +0200

    refactor formik wit c\new component from formik

--- src/pages/Form/FormPage.tsx
+++ src/pages/Form/FormPage.tsx
@@ -1,45 +1,13 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-// const SignupSchema = Yup.object().shape({
-//   name: Yup.string()
-//     .min(2, 'Too Short!')
-//     .max(70, 'Too Long!')
-//     .required('Required'),
-//   email: Yup.string().email('Invalid email').required('Required'),
-// });
-// const initialValues = {
-//   name: '',
-//   email: '',
-// };
-
-// const FormPage = () => (
-//   <>
-//     <h1>Signup</h1>
-//     <Formik
-//       initialValues={initialValues}
-//       validationSchema={SignupSchema}
-//       onSubmit={(values) => {
-//         console.log(values);
-//       }}
-//     >
-//       <Form>
-//         <Field name="name" />
-//         - + <ErrorMessage name="name" />
-//         <Field name="email" type="email" />
-//         - + <ErrorMessage name="email" />
-//         <button type="submit">Submit</button>
-//       </Form>
-//     </Formik>
-//   </>
-// );
 type InitialValue = {
-  name?: string;
-  email?: string;
+  name: string;
+  email: string;
 };
 
-const initialValues = {
+const initialValues: InitialValue = {
   name: '',
   email: '',
 };
@@ -54,45 +22,22 @@ const validationSchema = Yup.object().shape({
 });
 
 const FormPage = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
-
-  console.log(formik.touched);
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        // name="name"
-        // onChange={formik.handleChange}
-        // value={formik.values.name}
-        // onBlur={formik.handleBlur}
-        {...formik.getFieldProps('name')}
-      />
-      {formik.touched.name && formik.errors.name ? (
-        <div>{formik.errors.name}</div>
-      ) : (
-        ''
-      )}
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        // name="email"
-        // onChange={formik.handleChange}
-        // value={formik.values.email}
-        // onBlur={formik.handleBlur}
-        {...formik.getFieldProps('email')}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : (
-        ''
-      )}
-      <button>Submit</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <label htmlFor="name">Name</label>
+        <Field type="text" id="name" name="name" />
+        <ErrorMessage name="name" />
+        <label htmlFor="email">Email</label>
+        <Field type="text" id="email" name="email" />
+        <ErrorMessage name="email" />
+        <button>Submit</button>
+      </Form>
+    </Formik>
   );
 };
 export default FormPage;
