c6a00b1dfdcdc2dbc09df59f0b43274b677d3510
KlaraGajaszek <klaragajaszek21@wp.pl>
2021-03-09 13:22:10 +0100

    fix error with children

--- src/pages/Form/FormPage.tsx
+++ src/pages/Form/FormPage.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import axios from 'axios';
-import { Formik, Form, FormikValues } from 'formik';
-import * as Yup from 'yup';
-import { FormTemplate } from './FormTemplate';
+import React, { FC } from 'react'
+import axios from 'axios'
+import { Formik, Form, FormikHelpers } from 'formik'
+import * as Yup from 'yup'
+import { FormTemplate } from './FormTemplate'
 
 type InitialValue = {
-  name: string;
-  description: string;
-  image: string;
-  tags: string[];
-  price: string;
-};
+  name: string
+  description: string
+  image: string
+  tags: string[]
+  price: string
+}
 
 const initialValues: any = {
   name: '',
@@ -18,7 +18,7 @@ const initialValues: any = {
   image: '',
   tags: [''],
   price: '',
-};
+}
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label('Name'),
@@ -26,43 +26,43 @@ const validationSchema = Yup.object().shape({
   image: Yup.string().label('Image'),
   tags: Yup.string().label('Tags'),
   price: Yup.string().label('Price'),
-});
+})
 
-export const FormPage = () => {
-  // const formik: any = useFormikContext();
-  // const onSubmit = (values: any, onSubmitProps: FormikHelpers<any>) => {
-  //   console.log('values', values);
+export const FormPage: FC<any> = ({ children }: any) => {
+  // const formik: any = useFormikContext()
+  const onSubmit = (values: any, onSubmitProps: FormikHelpers<any>) => {
+    console.log('values', values)
 
-  // let bodyFormData = new FormData();
+    let bodyFormData = new FormData()
 
-  // bodyFormData.append('name', values.name);
-  // bodyFormData.append('description', values.description);
-  // bodyFormData.append('tags', JSON.stringify(values.tags));
-  // bodyFormData.append('price', values.price);
-  // bodyFormData.append('image', values.image);
+    bodyFormData.append('name', values.name)
+    bodyFormData.append('description', values.description)
+    bodyFormData.append('tags', JSON.stringify(values.tags))
+    bodyFormData.append('price', values.price)
+    bodyFormData.append('image', values.image)
 
-  // axios({
-  //   method: 'post',
-  //   url: `https://httpbin.org/post`,
-  //   headers: { 'Content-Type': 'multipart/form-data' },
-  //   data: bodyFormData,
-  //   onUploadProgress: (progressEvent) => {
-  //     console.log(
-  //       'upload event',
-  //       Math.round((progressEvent.loaded / progressEvent.total) * 100)
-  //     );
-  //   },
-  // })
-  //   .then(function () {
-  //     setTimeout(() => {
-  //       // handleFormModal(false);
-  //     }, 500);
-  //     onSubmitProps.resetForm();
-  //   })
-  //   .catch(function (response) {
-  //     //handle error
-  //   });
-  // };
+    axios({
+      method: 'post',
+      url: `https://httpbin.org/post`,
+      headers: { 'Content-Type': 'multipart/form-data' },
+      data: bodyFormData,
+      onUploadProgress: (progressEvent) => {
+        console.log(
+          'upload event',
+          Math.round((progressEvent.loaded / progressEvent.total) * 100)
+        )
+      },
+    })
+      .then(function () {
+        setTimeout(() => {
+          // handleFormModal(false);
+        }, 500)
+        onSubmitProps.resetForm()
+      })
+      .catch(function (response) {
+        //handle error
+      })
+  }
 
   return (
     <Formik
@@ -70,7 +70,7 @@ export const FormPage = () => {
       validationSchema={validationSchema}
       onSubmit={(values: any) => console.log(values)}
     >
-      {/* <Form>{children(<FormTemplate />)}</Form> */}
+      <Form>{children(<FormTemplate />)}</Form>
     </Formik>
-  );
-};
+  )
+}
